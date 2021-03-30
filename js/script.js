5b59560cd3f66e2dd03925d95f955a5c0cad3f67
unknown <jesper.addens@hotmail.com>
2021-03-30 10:21:22 +0200

    started working on responsive menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+  let getMenu = document.querySelector(".navbar");
+  let getMenuUl = document.querySelector(".navbar ul");
+  let getMenuA = document.querySelector(".navbar a");
+}
