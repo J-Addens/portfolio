f7bce21d2190369ec1e363d37b2730fb648c60e2
unknown <jesper.addens@hotmail.com>
2021-04-02 15:05:46 +0200

    started services section

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
   $('#icon').click(function(){
     $('ul').toggleClass('active');
   });
-})
+
+  $(window).scroll(function(){
+    if(this.scrollY >20){
+      $('.navbar').addClass('active');
+    } else {
+      $('.navbar').removeClass('active');
+    }
+  });
+});
